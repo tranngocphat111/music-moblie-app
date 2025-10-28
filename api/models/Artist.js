9d0c4610f33a1c394621797dbde0c29eaa6d5ecb
DarkThinh <139979897+DarkTzeoi@users.noreply.github.com>
2025-10-28 15:28:32 +0700

    update Artist and Song schemas to remove unused _id

--- api/models/Artist.js
+++ api/models/Artist.js
@@ -1,22 +1,27 @@
 // models/Artist.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Schema con cho album (lồng bên trong artist)
-const AlbumInfoSchema = new Schema({
+const AlbumInfoSchema = new Schema(
+  {
     album_id: Number,
     title: String,
-    release_date: String
-}, { _id: false }); // Tắt _id cho schema con
+    release_date: String,
+  },
+  { _id: false }
+); // Tắt _id cho schema con
 
 // Schema chính cho Artist
-const ArtistSchema = new Schema({
-    _id: String,
+const ArtistSchema = new Schema(
+  {
     artist_id: { type: Number, required: true, unique: true },
     name: { type: String, required: true },
     bio: String,
     image_url: String,
-    albums: [AlbumInfoSchema] // Một mảng chứa các album
-}, { collection: 'artists' }); // Tên collection trong database
+    albums: [AlbumInfoSchema], // Một mảng chứa các album
+  },
+  { collection: "artists" }
+); // Tên collection trong database
 
-module.exports = mongoose.model('Artist', ArtistSchema);
+module.exports = mongoose.model("Artist", ArtistSchema);
