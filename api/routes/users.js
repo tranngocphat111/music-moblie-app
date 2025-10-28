02d81d26b4082e7cc8e8d1373df2d344d6068568
DarkThinh <139979897+DarkTzeoi@users.noreply.github.com>
2025-10-28 19:29:06 +0700

    add middleware/auth to authenticateToken

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const User = require("../models/User"); // <-- 1. Import model
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const authenticateToken = require("../middleware/auth");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.get("/", async (req, res) => {
@@ -86,10 +86,8 @@ router.post("/sign-in", async (req, res) => {
 
 router.get("/me", authenticateToken, async (req, res) => {
   try {
-    const user = await User.findById(req.user._id).select("-password -__v");
-    if (!user) {
-      return res.status(404).json({ message: "Không tìm thấy người dùng." });
-    }
+    const user = await User.findById(req.user._id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
     res.json(user);
   } catch (err) {
     res.status(500).json({ message: err.message });
